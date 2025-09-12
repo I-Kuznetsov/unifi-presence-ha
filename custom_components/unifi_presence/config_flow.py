181458d5b2d598f31f133cc2f974945b9398cca5
Evgenich <78541210+I-Kuznetsov@users.noreply.github.com>
2025-09-12 13:29:00 +0300

    Update config_flow.py

--- custom_components/unifi_presence/config_flow.py
+++ custom_components/unifi_presence/config_flow.py
@@ -1,51 +1,54 @@
 from __future__ import annotations
 import voluptuous as vol
-import logging
 from homeassistant import config_entries
-from homeassistant.core import callback
 from .const import DOMAIN
-from .unifi_api import UniFiApi
 
-_LOGGER = logging.getLogger(__name__)
-
-DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required("host"): str,
-        vol.Required("username"): str,
-        vol.Required("password"): str,
-        vol.Optional("site", default="default"): str,
-        vol.Optional("verify_ssl", default=False): bool,
-    }
-)
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class UniFiPresenceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            # Проверка соединения с UniFi
-            try:
-                api = UniFiApi(
-                    host=user_input["host"],
-                    username=user_input["username"],
-                    password=user_input["password"],
-                    site=user_input["site"],
-                    verify_ssl=user_input["verify_ssl"],
-                )
-                await api.login()
-            except Exception as e:
-                errors["base"] = "connection_error"
-                _LOGGER.error("Connection error: %s", e)
-            else:
-                # Проверяем, нет ли уже конфигурации с таким хостом
-                await self.async_set_unique_id(user_input["host"])
-                self._abort_if_unique_id_configured()
-                
-                return self.async_create_entry(title="UniFi Presence", data=user_input)
+            # TODO: здесь можно добавить проверку соединения с UniFi
+            return self.async_create_entry(
+                title=f"UniFi Presence ({user_input['host']})",
+                data=user_input,
+            )
+
+        data_schema = vol.Schema(
+            {
+                vol.Required("host"): str,
+                vol.Required("username"): str,
+                vol.Required("password"): str,
+                vol.Optional("site", default="default"): str,
+                vol.Optional("verify_ssl", default=False): bool,
+            }
+        )
 
         return self.async_show_form(
-            step_id="user", 
-            data_schema=DATA_SCHEMA, 
-            errors=errors
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors,
         )
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        return OptionsFlowHandler(config_entry)
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        options_schema = vol.Schema(
+            {
+                vol.Optional(
+                    "update_interval", default=self.config_entry.options.get("update_interval", 30)
+                ): int
+            }
+        )
+        return self.async_show_form(step_id="init", data_schema=options_schema)
